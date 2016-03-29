97d06f25c16f42b42f2225c8104399c389068af6
teamuser1 <ajinkya@afybiz.net>
2016-03-29 19:52:46 +0530

    full

---
+++ teamproject1/teamproject1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="teamproject1.Global" Language="C#" %>
---
+++ teamproject1/teamproject1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(teamproject1.Startup))]
+namespace teamproject1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
